5577c0ecf3ca6e8dce889d334e95696b3f000250
Will Ayd <william.ayd@icloud.com>
2022-08-17 19:57:16 -0700

    work graphql

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,14 +1,33 @@
 import React from "react";
-import image1 from "../images/image001.jpg";
-import image2 from "../images/image002.jpg";
-import image3 from "../images/image003.jpg";
-import image4 from "../images/image004.jpg";
-import image5 from "../images/image005.jpg";
-import image6 from "../images/image006.jpg";
-import image7 from "../images/image007.jpg";
-import image8 from "../images/image008.jpg";
+import { graphql } from "gatsby"
+
 
 const Portfolio = () => {
+  const query = graphql`
+  query {
+    contentfulAlisonGallionPhotography {
+      portfolioImages {
+        id
+        title
+        url
+        width
+        height
+      }
+    }
+  }
+`;
+  const images = query["data"]["contentfulAlisonGallionPhotography"]["portfolioImages"];
+  const imageHtml = images.map((image) => {
+    return (
+      <div className="group item">
+        <img
+	  src={image["url"]}
+          alt=""
+          className="w-full duration-200 md:block group-hover:scale-110"
+        />
+      </div>
+    )});
+
   return (
     <section id="portfolio">
       <div className="container max-w-6xl mx-auto my-32 px-6 text-gray-900 md:px-0">
@@ -19,71 +38,7 @@ const Portfolio = () => {
         </div>
 
         <div className="item-container">
-          <div className="group item">
-            <img
-	      src={image1}
-              alt=""
-              className="w-full duration-200 md:block group-hover:scale-110"
-            />
-          </div>
-
-          <div className="group item">
-            <img
-              src={image2}
-              alt=""
-              className="w-full duration-200 md:block group-hover:scale-110"
-            />
-          </div>
-
-          <div className="group item">
-            <img
-              src={image3}
-              alt=""
-              className="w-full duration-200 md:block group-hover:scale-110"
-            />
-          </div>
-
-          <div className="group item">
-            <img
-              src={image4}
-              alt=""
-              className="w-full duration-200 md:block group-hover:scale-110"
-            />
-          </div>
-        </div>
-
-        <div className="item-container mt-10">
-          <div className="group item">
-            <img
-              src={image5}
-              alt=""
-              className="w-full duration-200 md:block group-hover:scale-110"
-            />
-          </div>
-
-          <div className="group item">
-            <img
-              src={image6}
-              alt=""
-              className="w-full duration-200 md:block group-hover:scale-110"
-            />
-          </div>
-
-          <div className="group item">
-            <img
-              src={image7}
-              alt=""
-              className="w-full duration-200 md:block group-hover:scale-110"
-            />
-          </div>
-
-          <div className="group item">
-            <img
-              src={image8}
-              alt=""
-              className="w-full duration-200 md:block group-hover:scale-110"
-            />
-          </div>
+	  {imageHtml}
         </div>
       </div>
     </section>

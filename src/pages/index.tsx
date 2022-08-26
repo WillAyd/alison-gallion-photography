8fa056a10069f39a35ceae088c926e46147a129c
Will Ayd <william.ayd@icloud.com>
2022-08-26 14:49:54 -0700

    local font; meta for seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,31 @@
-import * as React from "react"
-import "../style/global.css"
-import type { HeadFC } from "gatsby"
-import Hero from "../components/Hero"
-import AboutMe from "../components/AboutMe"
-import Portfolio from "../components/Portfolio"
-import Footer from "../components/Footer"
+import * as React from "react";
+import "@fontsource/josefin-sans/300.css";
+import "../style/global.css";
+import type { HeadFC } from "gatsby";
+import Hero from "../components/Hero";
+import AboutMe from "../components/AboutMe";
+import Portfolio from "../components/Portfolio";
+import Footer from "../components/Footer";
 
 const IndexPage = () => {
   return (
-    <React.Fragment><Hero /><AboutMe /><Portfolio /><Footer /></React.Fragment>
-  )
-}
+    <React.Fragment>
+      <Hero />
+      <AboutMe />
+      <Portfolio />
+      <Footer />
+    </React.Fragment>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
 export const Head: HeadFC = () => (
   <React.Fragment>
     <title>Alison Gallion Photography</title>
-    <link
-      href="https://fonts.googleapis.com/css2?family=Alata&family=Josefin+Sans:wght@300&display=swap"
-      rel="stylesheet"
-    />    
+    <meta
+      name="description"
+      content="Alison Gallion is a photographer in San Diego offering family photos, pet photos, professional headshots and wedding photography."
+    />
   </React.Fragment>
-)
+);
